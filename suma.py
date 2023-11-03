d85d9f32c5f66a4ecb9169290902020b58d6bdf1
John <90121344+JDC39@users.noreply.github.com>
2023-11-02 20:57:02 -0500

    modifico en input varaible c numero por digito

--- suma.py
+++ suma.py
@@ -1,15 +1,4 @@
 a = int(input("ingresa un numero: "))
 b = int(input("ingresa un numero: "))
-c = int(input("ingresa un numero: "))
+c = int(input("ingresa un digito: "))
 
-def sum(a,b,c):
-    suma = a + b + c
-    print(suma)
-
-def res(a,c):
-    resta = a - c
-    print(resta)    
-
-def prod(a,b):
-    producto = a * b
-    print(producto)
